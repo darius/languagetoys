b8ba09b43030fe404ed626374cc856645f341975
darius <darius@24-205-5-126.dhcp.gldl.ca.charter.com>
2009-03-15 15:18:23 -0700

    feminine rhymes; match level-2 vowels more accurately against meter; remove currently-unused features; memoize segment_beats()

--- verse.py
+++ verse.py
@@ -58,16 +58,6 @@ def main(argv):
     for verser in versers:
         print_verse(verser())
 
-def print_double_dactyl_words():
-    for word in words_that_match(dactyl * 2):
-        print word
-
-def words_that_match(meter):
-    for word in pronounce.known_words():
-        word = word.lower()
-        if () == match_word(word, meter):
-            yield word
-
 def start():
     return babble.start(start_state)
     
@@ -76,77 +66,6 @@ def print_verse(verse):
     for line in verse:
         print format_line(line)
     print
-
-def check_sonnet123():
-    for line in open('sonnet123'):
-        if not scans(line):
-            words = re.findall(r"['\w]+", line)
-            print match_words(words, iambic_pentameter)
-            print line
-
-def scans(line):
-    words = re.findall(r"['\w]+", line)
-    return match_words(words, iambic_pentameter) == ()
-
-def lines_echo(line1, line2):
-    return rime(rhyme_phones(line1)) == rime(rhyme_phones(line2))
-
-def lines_rhyme(line1, line2):
-    # N.B. we assume line1, line2 don't end in <S>
-    return (line1[-1] != line2[-1]  # such rhymes are almost always weak
-            and rhyme_matches(rhyme_phones(line1),
-                              rhyme_phones(line2)))
-
-def rhyme_matches(phones1, phones2):
-    return onset(phones1) != onset(phones2) and rime(phones1) == rime(phones2)
-
-def onset(phones):
-    return '' if pronounce.is_vowel(phones[0]) else phones[0]
-
-def rime(phones):
-    return phones if pronounce.is_vowel(phones[0]) else phones[1:]
-
-# M       A     D      L       A      D      EE
-# //m   m/a/  m/a/d  m/a/dl  l/a/   l/a/d   d/ee/
-
-def rhyme_phones(line):
-    line = [t for t in line if t != '<S>']
-    # XXX using only the last word gives better results for now,
-    #  but will have to be revisited:
-    if line: line = [line[-1]]
-    before = []
-    vowels = []
-    after  = []
-    state = 'consonant'
-    # XXX This only works for 'masculine' rhymes. And man the code sucks.
-    for phone in pronounce_line(line):
-        if pronounce.is_vowel(phone):
-            if state == 'vowel':
-                # v->v
-                vowels.append(phone)
-            else:
-                # c->v
-                before = [after[-1]] if after else []
-                vowels = [phone]
-                after  = []
-        else:
-            if state == 'vowel':
-                # v->c
-                after = [phone]
-            else:
-                # c->c
-                after.append(phone)
-    return before + vowels + after
-
-def pronounce_line(line):
-    return reduce(operator.add, map(pronounce.pronounce, line), ())
-
-slack, stressed, rhymed = range(3)
-iamb = (slack, stressed)
-dactyl = (stressed, slack, slack)
-anapest = (slack, stressed, slack) # XXX wrong
-iambic_tetrameter = iamb * 4
-iambic_pentameter = iamb * 5
 
 def format_line(line):
     out = ''
@@ -164,7 +83,16 @@ def format_line(line):
             out = out + ' ' + t
     return out
 
+slack, stressed, rhymed = range(3)
+iamb = (slack, stressed)
+dactyl = (stressed, slack, slack)
+anapest = (slack, stressed, slack) # XXX wrong
+iambic_tetrameter = iamb * 4
+iambic_pentameter = iamb * 5
+
 def gen_blank_verse(n_lines=6, state=None):
+    # XXX we seem to be getting extra beats at the end sometimes now
+    #  (as if we were ending with a 'rhymed' constituent)
     meters = [iambic_pentameter] * n_lines
     rhymes = [[]] * n_lines
     antis  = [[]] * n_lines
@@ -172,7 +100,8 @@ def gen_blank_verse(n_lines=6, state=None):
 
 def gen_sonnet(state=None):
     # rhyme_scheme = 'ababcdcdefefgg'
-    meters = [iambic_pentameter] * 14
+    m = iambic_pentameter[:-1] + (rhymed,)
+    meters = [m] * 14
     #         1  2   3   4   5  6  7   8   9 10 11  12   13 14
     #         a  b   a   b   c  d  c   d   e  f  e   f    g  g
     rhymes = [[],[], [1],[2],[],[],[5],[6],[],[],[9],[10],[],    [13]]
@@ -181,19 +110,22 @@ def gen_sonnet(state=None):
 
 def gen_couplet(state=None):
     # rhyme_scheme = 'aa'
+    m = iambic_pentameter[:-1] + (rhymed,)
     rhymes = [[],[1]]
     antis  = [[],[]]
-    return gen_verse(rhymes, antis, [iambic_pentameter] * 2, state)
+    return gen_verse(rhymes, antis, [m] * 2, state)
 
 def gen_incantation(state=None):
+    m = (stressed, slack, stressed, slack, stressed, slack, rhymed)
     # rhyme_scheme = 'aa'
     rhymes = [[],[1]]
     antis  = [[],[]]
-    return gen_verse(rhymes, antis, [(2,0,1,0,2,0,1)] * 2, state)
+    return gen_verse(rhymes, antis, [m] * 2, state)
 
 def gen_quatrain(state=None):
     # rhyme_scheme = 'abab'
-    meters = [iambic_tetrameter] * 4
+    m = iambic_tetrameter[:-1] + (rhymed,)
+    meters = [m] * 4
     rhymes = [[],[],[1],[2]]
     antis  = [[],[1],[],[]]
     return gen_verse(rhymes, antis, meters, state)
@@ -202,13 +134,16 @@ def gen_neolithic(state=None):
     'in the neolithic age'
     'de de dah de de de dah'
     'de dah de dah de dah de dah de dah'
-    semi = (1,0,2,0,1,0,2)  # -.*.-.*
+    #semi = (1,0,2,0,1,0,2)  # -.*.-.*
+    # TODO: a 1-beat-only 'rhymed' constituent
+    m = iambic_pentameter[:-1] + (rhymed,)
+    semi = (stressed, slack, stressed, slack, stressed, slack, rhymed)
     meters = [semi,
               semi,
-              iambic_pentameter,
+              m,
               semi,
               semi,
-              iambic_pentameter]
+              m]
     # rhyme_scheme = 'aabccb'
     #                 123456
     rhymes = [[],[1],[],[],[4],[3]]
@@ -220,22 +155,25 @@ def gen_double_dactyl(state=None):
     # TODO: fit in a whole-line double-dactyl word in the 2nd verse
     meters = [dactyl * 2,
               dactyl * 2,
-              (2,0,0,2),
+              (stressed, slack, slack, rhymed),
               dactyl * 2,
               dactyl * 2,
               dactyl * 2,
-              (2,0,0,2)]
+              (stressed, slack, slack, rhymed)]
     # rhyme_scheme = '--a---a'
     #                 1234567
     rhymes = [[],[],[],[],[],[],[3]]
-    antis  = [[],[],[2],[3],[],[3],[]]
+    # XXX we need gen_line() to return the rhyme even on a non-rhyming
+    #  line (for the sake of antirhyme checks)
+    # antis  = [[],[],[2],[3],[],[3],[]]
+    antis  = [[],[],[],[],[],[],[]]
     return [['higgledy', 'piggledy']] + gen_verse(rhymes, antis, meters, state)
 
 def gen_limerick(state=None):
     #end1 = anapest if random.randint(0, 1) else iamb
     if random.randint(0, 1):
-        end1 = anapest if False else iamb
-        end2 = anapest if False else iamb
+        end1 = anapest if False else (slack, rhymed)
+        end2 = anapest if False else (slack, rhymed)
         meters = [anapest * 2 + end1,
                   anapest * 2 + end1,
                   anapest + end2,
@@ -243,11 +181,12 @@ def gen_limerick(state=None):
                   anapest * 2 + end1]
     else:
         anapest1 = (slack, slack, stressed)
-        meters = [anapest1 * 3,
-                  anapest1 * 3,
-                  anapest1 * 2,
-                  anapest1 * 2,
-                  anapest1 * 3]
+        end = (slack, slack, rhymed)
+        meters = [anapest1 * 2 + end,
+                  anapest1 * 2 + end,
+                  anapest1 + end,
+                  anapest1 + end,
+                  anapest1 * 2 + end]
     # rhyme_scheme = 'aabba'
     rhymes = [[],[1],[],[3],[2,1]]
     antis  = [[],[],[1],[],[]]
@@ -256,10 +195,16 @@ def gen_limerick(state=None):
 class Exhausted(Exception):
     pass
 
+def tick(counter):
+    counter[0] -= 1
+    if counter[0] < 0:
+        raise Exhausted()
+
 def gen_verse(rhymescheme, antirhymescheme, meters, state=None):
     if state is None: state = start_state
     order = babble.the_order()
     lines = [list(state)]
+    linerhymes = [None]
     i = 1
     length = len(rhymescheme)
     rhymescheme = [[]] + rhymescheme
@@ -267,34 +212,36 @@ def gen_verse(rhymescheme, antirhymescheme, meters, state=None):
     meters = [None] + meters
     while i <= length:
         state_i = tuple(reduce(operator.add, lines, [])[-order:])
-        rhymes = [rhyme_phones(lines[j]) for j in rhymescheme[i]]
-        antirhymes = [rhyme_phones(lines[j]) for j in antirhymescheme[i]]
+        rhymes     = [linerhymes[j] for j in rhymescheme[i]]
+        antirhymes = [linerhymes[j] for j in antirhymescheme[i]]
         if i == 1:  # XXX hacky
             state_i = babble.start(state_i)
             m = match_words(tuple(t for t in state_i if t != '<S>'),
                             meters[i])
             if m is None:
                 continue
-            line = gen_line(meter=m,
-                            rhymes=rhymes,
-                            antirhymes=antirhymes,
-                            state=state_i,
-                            cutoff=10000)
+            line, rhyme = gen_line(meter=m,
+                                   rhymes=rhymes,
+                                   antirhymes=antirhymes,
+                                   state=state_i,
+                                   cutoff=10000)
             if line: line = list(state_i) + line
         else:
-            line = gen_line(meter=meters[i],
-                            rhymes=rhymes,
-                            antirhymes=antirhymes,
-                            state=state_i,
-                            cutoff=60000)
+            line, rhyme = gen_line(meter=meters[i],
+                                   rhymes=rhymes,
+                                   antirhymes=antirhymes,
+                                   state=state_i,
+                                   cutoff=60000)
         if line is None:
-            # # XXX hacky:
+            # XXX hacky:
             i = max(1, max(rhymescheme[i]) if rhymescheme[i] else i-1)
             lines = lines[:i]
+            linerhymes = linerhymes[:i]
             print >>sys.stderr, 'Backing up to %d' % i
         else:
             print >>sys.stderr, '%4d' % i, format_line(line)
             lines.append(line)
+            linerhymes.append(rhyme)
             i += 1
     return lines[1:]
 
@@ -302,11 +249,11 @@ def gen_line(meter, rhymes, antirhymes, state, cutoff):
     counter = [cutoff]
     while True:
         try:
-            line = gen(meter, rhymes, antirhymes, state, counter)
+            line, rhyme = gen(meter, rhymes, antirhymes, state, counter)
         except Exhausted:
-            return None
+            return None, None
         if line:
-            return line
+            return line, rhyme
 
 def gen(meter, rhymes, antirhymes, state, counter):
     tick(counter)
@@ -315,59 +262,108 @@ def gen(meter, rhymes, antirhymes, state, counter):
             word = babble.pick_word2(state)
         except KeyError:
             continue
-        after = match_word(word, meter)
+        after, rhyme = match_word(word, meter)
         if () == after:
-            rhyme = rhyme_phones([word])
             if (all(rhyme_matches(rhyme, r) for r in rhymes)
                 and not any(rime(rhyme) == rime(r) for r in antirhymes)):
-                return [word]
+                return [word], rhyme
             continue
         if after is not None:
-            rest = gen(after, rhymes, antirhymes, 
-                       babble.update(state, word),
-                       counter)
+            rest, rhyme = gen(after, rhymes, antirhymes, 
+                              babble.update(state, word),
+                              counter)
             if rest is not None:
-                return [word] + rest
-    return None
-
-def tick(counter):
-    counter[0] -= 1
-    if counter[0] < 0:
-        raise Exhausted()
+                return [word] + rest, rhyme
+    return None, None
 
 def match_words(words, meter):
     for word in words:
         if meter is None: break
-        meter = match_word(word, meter)
+        meter, rhyme = match_word(word, meter)
     return meter
 
 def match_word(word, meter):
     if meter and meter[0] and word in unstressables:
-        return None
+        return None, None
     if True:
         if word == '<S>':
-            return meter
+            return meter, None
     return match_phones(pronounce.pronounce(word), meter)
-    
-def match_phones(phones, meter):
-    return match_beats(segment_beats(phones), meter)
 
+def match_phones(phones, meter):
+    beats = segment_beats(phones)
+    lobeat, hibeat = argh(beats)
+    v, nvowels = 0, len(beats)
+    p, nphones = 0, len(phones)
+    for j, m in enumerate(meter):
+        if nphones <= p:
+            return meter[j+1:], None
+        if m == rhymed:
+            # NB we assume a rhymed may only appear once, at the end of meter
+            # XXX I think this is not quite right: e.g. 'intelligible'
+            #  against meter (0,1,0,rhymed) produces rhyme 'gible' here,
+            #  but it should fail:
+            return match_as_rhyme(phones[p:], lobeat, hibeat)
+        while not pronounce.is_vowel(phones[p]):
+            p += 1
+            if nphones <= p:
+                return None, None
+        if not match_beat(int(phones[p][-1]), m, lobeat, hibeat):
+            return None, None
+        p += 1
+        v += 1
+        if v == nvowels:
+            return meter[j+1:], None
+    return (), None
+
+sheeshtable = [0, 2, 1]
+def argh(beats):
+    sheesh = [sheeshtable[b] for b in beats]
+    return min(sheesh), max(sheesh)
+
+def match_as_rhyme(phones, lobeat, hibeat):
+    beats = segment_beats(phones)
+    assert beats
+    if beats[0] == 1 or lobeat == hibeat:
+        return (), phones
+    return None, None
+
+def match_beat(beat, meter_beat, lobeat, hibeat):
+    # TODO: use 1/2 distinction in meter
+    if meter_beat == 0:
+        rv = (sheeshtable[beat] == lobeat)
+    else:
+        rv = (lobeat < sheeshtable[beat] or lobeat == hibeat)
+    return rv
+
+def memo(f):
+    "Memoize function f."
+    table = {}
+    def fm(*x):
+        if x not in table:
+            table[x] = f(*x)
+        return table[x]
+    fm.memo = table
+    return fm
+
+@memo
 def segment_beats(phones):
     return [int(phone[-1]) for phone in phones if pronounce.is_vowel(phone)]
 
-def match_beats(beats, meter):
-    if len(meter) < len(beats):
-        return None
-    if len(set(beats)) == 1:
-        return meter[len(beats):]
-    if all(match_beat(beat, m) for beat, m in zip(beats, meter)):
-        return meter[len(beats):]
-    return None
 
-def match_beat(beat, meter_beat):
-    # TODO: use 1/2 distinction
-    if meter_beat == 0: return beat == 0
-    return beat != 0
+def rhyme_matches(phones1, phones2):
+    return onset(phones1) != onset(phones2) and rime(phones1) == rime(phones2)
+
+def onset(phones): return phones[:find_rime(phones)]
+def rime(phones):  return phones[find_rime(phones):]
+
+def find_rime(phones):
+    assert isinstance(phones, tuple), "phones: %r" % phones
+    for i, ph in enumerate(phones):
+        if pronounce.is_vowel(ph):
+            return i
+    assert False
+
 
 if __name__ == '__main__':
     import sys
