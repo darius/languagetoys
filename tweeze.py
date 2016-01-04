6ed09c59b489565e68504389ca18122a1b48dd37
Darius Bacon <darius@wry.me>
2016-01-04 01:14:20 -0500

    preserve rhymes at end of lines

--- tweeze.py
+++ tweeze.py
@@ -1,9 +1,12 @@
 """
 Try to produce manglings like "Tweeze denied beef worker isthmus".
 """
+import re
+from math import log10
+
 from memo import memo
 from pdist import Pw
-from math import log10
+from simpleverse import find_rime
 
 roughener = {
     'd': 't', 'dh': 't', 'th': 't',
@@ -37,28 +40,23 @@ def pronounce(word):
 def pronounce_all(words):
     return sum(map(pronounce, words), ())
 
-def annotated_pronounce_all(words):
+def pronounce_line(words):
     phones, bounds = (), ()
     for word in words:
-        i = len(phones)
         p = pronounce(word)
         phones += p
         bounds += (word,) + (None,)*(len(p)-1)
     return phones, bounds
 
+def roughen_line(phones):
+    "Roughen phones, except preserving the rime at the end."
+    i = find_rime(phones)
+    return roughen(phones[:i]) + phones[i:]
+
 ## pronounce('yay')
 #. ('y', 'ey1')
 ## pronounce_all(('darius',))
 #. ('d', 'er0', 'ay1', 'ah0', 's')
-## annotated_pronounce_all(('darius','is','great'))
-#. (('d', 'er0', 'ay1', 'ah0', 's', 'ih1', 'z', 'g', 'r', 'ey1', 't'), ('darius', None, None, None, None, 'is', None, 'great', None, None, None))
-
-## transcribe(*annotated_pronounce_all(tuple("stirring not even a mouse".split())))
-#. (21.741929954664087, ('staying', 'note', 'ave', 'mass'))
-## transcribe(*annotated_pronounce_all(tuple("snug in their beds with visions".split())))
-#. (43.36161186892771, ('snag', "'n", 'there', 'bids', 'wythe', 'versions'))
-## transcribe(*annotated_pronounce_all(tuple("syne".split())))
-#. (56.71069362546324, ('syne',))
 
 longest = 20
 match_cost = 25
@@ -66,13 +64,10 @@ fit_cost = 5
 rarity_cost = 5
 roughened_cost = 5
 
-## transcribe(*annotated_pronounce_all(tuple("the light".split())))
-#. (56.50999260901199, ('the', 'let'))
-
 ## pronounce_all(tuple("the light".split()))
 #. ('dh', 'ah0', 'l', 'ay1', 't')
 
-def compute_best(phones, bounds, costs, seqs, i):
+def compute_best(phones, rough_phones, bounds, costs, seqs, i):
     attempts = []
     for L in range(1, min(i, longest) + 1):
         assert len(phones[:i-L]) < len(phones)
@@ -84,19 +79,19 @@ def compute_best(phones, bounds, costs, seqs, i):
         exacts = words_of_phones.get(phones[i-L:i], ())
         for word in exacts:
             add(word, subcost)
-        for word in rough_words.get(roughen(phones[i-L:i]), ()):
+        for word in rough_words.get(rough_phones[i-L:i], ()):
             if word not in exacts:
                 add(word, subcost + roughened_cost)
     return min(attempts) if attempts else (1e6, ('XXX',))
 
-def transcribe(phones, bounds):
+def transcribe(phones, rough_phones, bounds):
     """Return (cost,words) where pronounce_all(words) matches
     `phones`. `cost` is lower the better the match. Try to find the
     lowest cost."""
     assert len(phones) == len(bounds)
     costs, seqs = [0], [()]
     for i in range(1, len(phones) + 1):
-        cost, seq = compute_best(phones, bounds, costs, seqs, i)
+        cost, seq = compute_best(phones, rough_phones, bounds, costs, seqs, i)
         costs.append(cost)
         seqs.append(seq)
     return costs[-1], seqs[-1]
@@ -109,14 +104,23 @@ def transcribe(phones, bounds):
 ## -log10(Pw('dar'))
 #. 5.644601987828583
 
+def pronounce_rhyming_lines(lines):
+    phones, rough_phones, bounds = (), (), ()
+    for line in lines:
+        p, b = pronounce_line(re.findall(r"['\w]+", line.lower()))
+        phones += p
+        rough_phones += roughen_line(p)
+        bounds += b
+    return phones, rough_phones, bounds
+
 def main():
-    import re, sys, textwrap
-    text = re.findall(r"['\w]+", sys.stdin.read().lower())
-    phones, bounds = annotated_pronounce_all(text)
+    import sys, textwrap
+    phones, rough_phones, bounds = pronounce_rhyming_lines(sys.stdin)
 #    print text
 #    print phones
+#    print rough_phones
 #    print len(phones)
-    cost, words = transcribe(phones, bounds)
+    cost, words = transcribe(phones, rough_phones, bounds)
     print cost
     print textwrap.fill(' '.join(words).lower(), 60)
 
